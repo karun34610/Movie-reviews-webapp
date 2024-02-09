1a56ec7562573a117169bafeadad897208748cbe
Karun <karun34610@gmail.com>
2024-02-09 22:58:33 +0530

    init app

--- frontend/src/backendUrl.ts
+++ frontend/src/backendUrl.ts
@@ -1,2 +1,2 @@
 // export const backend="http://52.65.32.122:80/";
-export const backend="https://saasmonk-server.onrender.com/";
+export const backend="https://movie-review-server-4s1g.onrender.com/";
